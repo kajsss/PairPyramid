479f2a8b58ef430c57386add66b021a127705acb
Lamos Kim and Steve Jeong <pair+lamos+steve@gmail.com>
2017-06-28 09:44:02 +0900

    skeleton mvp

--- app/src/main/java/msl/com/pairpyramid/view/main/presenter/MainContract.kt
+++ app/src/main/java/msl/com/pairpyramid/view/main/presenter/MainContract.kt
@@ -1,2 +1,17 @@
 package msl.com.pairpyramid.view.main.presenter
 
+interface MainContract{
+
+    interface View {
+        fun showPairStatus()
+        fun updatePairStatus()
+        fun showErrorMessage(message: String)
+        fun startPairMatchingActivity()
+    }
+
+    interface Presenter {
+        fun getPairStatus()
+
+
+    }
+}
